f9918c14a9afc4ac45397be004d2722c81e061cf
Dalaskor <dalas.prog@gmail.com>
2023-02-27 11:54:36 +0400

    update

--- hw-1/task_2.js
+++ hw-1/task_2.js
@@ -1,17 +1,17 @@
 const sumNums = (a = 0, b = 0) => {
-	return a + b;
+	return Number(a) + Number(b);
 }
 
 const mulNums = (a = 0, b = 0) => {
-	return a * b;
+	return Number(a) * Number(b);
 }
 
 const subNums = (a = 0, b = 0) => {
-	return a - b;
+	return Number(a) - Number(b);
 }
 
 const divNums = (a = 0, b = 0) => {
-	return b != 0 ? a / b : NaN;
+	return b != 0 ? Number(a) / Number(b) : NaN;
 }
 
 // export {sumNums, mulNums, subNums, divNums}
